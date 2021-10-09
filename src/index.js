62b8d82347557af03e4165590b44302cd00f6853
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-10-08 19:41:58 -0500

    Template and buttons to print image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 const URL_API = 'https://source.unsplash.com/random'
 
-console.log('Happy hacking .l. :)')
+const nodeAPP = document.querySelector('#images')
+
+nodeAPP.addEventListener('click', (event) => {
+  if (event.target.className === 'button-image') {
+    printImage();
+  } else {
+    console.log(`Hola desde ${event.target.nodeName}`);
+  }
+}
+)
+
+const buttonImage = document.querySelectorAll('.button-image')
+const buttonClear = document.querySelectorAll('.button-clear')
+
+
+const printImage = () => {
+  
+  const img = document.createElement('img')
+  img.style = 'width: 330px;'
+  img.className = 'mx-auto'
+
+  const div = document.createElement('div')
+  div.className = 'p-4 mx-auto'
+  div.style = 'width: 330px'
+
+  img.srcset = `${URL_API}`
+
+  div.append(img);
+
+  nodeAPP.insertBefore(div,buttons)
+}
