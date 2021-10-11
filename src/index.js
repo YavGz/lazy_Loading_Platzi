078e91c0211fec6be128d4ae910733dc7e490588
Javier Gutierrez (Yav) <gutierrez.alcantara.javier@gmail.com>
2021-10-11 13:57:06 -0500

    Implementación de lazy loading

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ const nodeAPP = document.querySelector('#images')
 
 
 //crear una funcion para obtener un random 
-const min = 1 /*Minimo de elementos del array*/ 
-const max = 122 /*Maximo de elementos del array*/
-const random = () => {
-  Math.floor(Math.random() * (max - min) + min)
-}
+// const min = 1 /*Minimo de elementos del array*/ 
+// const max = 122 /*Maximo de elementos del array*/
+// const random = () => {
+//   Math.floor(Math.random() * (max - min) + min)
+// }
 
 nodeAPP.addEventListener('click', (event) => {
   if (event.target.className === 'button-image') {
     printImage();
-  } else {
-    console.log(`Hola desde ${event.target.nodeName}`);
   }
 }
 )
@@ -30,15 +28,15 @@ const printImage = () => {
   const img = document.createElement('img')
   img.style = 'width: 330px;'
   img.className = 'mx-auto'
-  registerImage(img) /*Agregar la imagen al lazy*/
-
+  img.dataset.src = `${URL_API}`
+  
   const div = document.createElement('div')
   div.className = 'p-4 mx-auto'
   div.style = 'width: 330px'
-
-  img.srcset = `${URL_API}`
-
+  
   div.append(img);
+  
+  registerImage(img) /*Agregar la imagen al lazy*/
 
   nodeAPP.insertBefore(div,buttons)
 }
--- src/lazy.js
+++ src/lazy.js
@@ -2,16 +2,17 @@ const isIntersecting = (entry) => {
   return entry.isIntersecting;
 }
 
-const accion = (entry) => {
-  const nodo = entry.target;
-  console.log('Hey');
-
+const loadImage = (entry) => {
+  const imagen = entry.target; /**/
+  const url = imagen.dataset.src
+  // cargar imagen
+  imagen.src = url
   // quita el registro de la imagen (unlisten)
-  observer.unobserve(nodo)
+  observer.unobserve(imagen)
 }
 
 const observer = new IntersectionObserver((entries) => {
-  entries.filter(isIntersecting).forEach(accion)
+  entries.filter(isIntersecting).forEach(loadImage)
 })
 
 export const registerImage = (imagen) => {
