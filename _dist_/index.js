e163874895bb2d078b94aa2aa6040c1a2b218d92
Javier-GT-Yav <Javier-GT-Yav@users.noreply.github.com>
2021-10-15 00:37:52 +0000

    deploy: 2c331783bf71fc40b4ceee72c10cae4eb795986e

---
+++ _dist_/index.js
@@ -0,0 +1,54 @@
+import { registerImage } from "./lazy.js";
+
+
+const nodeAPP = document.querySelector('#container')
+
+const images = document.querySelector('#images')
+
+// crear una funcion para obtener un random 
+const min = 1 /*Minimo de elementos del array*/ 
+const max = 123 /*Maximo de elementos del array*/
+const random = () => Math.floor(Math.random() * (max - min) + min)
+
+
+nodeAPP.addEventListener('click', (event) => {
+  if (event.target.className === 'button-image btn btn-outline-primary') {
+    printImage();
+  }
+  else if (event.target.className === 'button-clear btn btn-outline-secondary') {
+    clearNodes();
+  }
+})
+
+const buttonImage = document.querySelectorAll('.button-image')
+const buttonClear = document.querySelectorAll('.button-clear')
+
+
+const printImage = () => {
+  
+  const img = document.createElement('img')
+  img.style = 'width: 330px;'
+  img.className = 'mx-auto imagen'
+  img.dataset.src = `https://randomfox.ca/images/${random()}.jpg`
+  
+  const imageWrapper = document.createElement("div");
+  imageWrapper.className = "bg-gray-300 imagen";
+  imageWrapper.style.minHeight = "160px";
+  imageWrapper.style.display = "inline-block";
+
+  const div = document.createElement('div')
+  div.className = 'p-4 mx-auto'
+  div.style = 'width: 300px;'
+  
+  imageWrapper.appendChild(img);
+
+  div.append(imageWrapper);
+  
+  registerImage(img) /*Agregar la imagen al lazy*/
+
+  images.append(div)
+}
+
+const clearNodes = () => {
+  images.innerHTML = ''
+}
